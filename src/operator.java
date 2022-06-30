26745cd5342f7f15fc0e98e13285375f372726b7
Александр Масляков <lapostov@yandex.ru>
2022-06-30 23:10:24 +0400

    Changed 1000 to 110

--- src/operator.java
+++ src/operator.java
@@ -1,11 +1,11 @@
 public class operator {
 
     public static void main(String[] args) {
-        int amount = 5000;
+        int amount = 1100;
         int bonus = amount / 100;
         int balance = 100 + amount + bonus;
         int x = 100;
-        if (amount >= 1000) {
+        if (amount >= 1100) {
             System.out.println("Ваш баланс " + balance + " рублей," + " бонусных из них " + bonus + " рублей");
         } else {
             System.out.println(x + amount + " рублей на Вашем счету");
