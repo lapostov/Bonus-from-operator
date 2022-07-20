10b87db1a618f2d4d85b70015609ac25ddba573d
Александр Масляков <lapostov@yandex.ru>
2022-07-20 14:27:20 +0400

    Variable changed from x to check

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
-public class main {
+public class Main {
 
     public static void main(String[] args) {
-        int amount = 1100;
+        int amount = 5000;
         int bonus = amount / 100;
         int balance = 100 + amount + bonus;
-        int x = 100;
+        int check = 100;
         if (amount >= 1100) {
             System.out.println("Ваш баланс " + balance + " рублей," + " бонусных из них " + bonus + " рублей");
         } else {
-            System.out.println(x + amount + " рублей на Вашем счету");
+            System.out.println(check + amount + " рублей на Вашем счету");
         }
 
     }
