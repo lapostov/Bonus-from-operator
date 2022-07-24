8cf6206c489b897646e45e5f216dfabf0c3e0655
Александр Масляков <lapostov@yandex.ru>
2022-07-24 23:17:29 +0400

    Changed condition

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
 
     public static void main(String[] args) {
-        int amount = 5000;
+        int amount = 1050;
         int bonus = amount / 100;
         int balance = 100 + amount + bonus;
         int check = 100;
-        if (amount >= 1100) {
+        if (amount >= 1001) {
             System.out.println("Ваш баланс " + balance + " рублей," + " бонусных из них " + bonus + " рублей");
         } else {
             System.out.println(check + amount + " рублей на Вашем счету");
